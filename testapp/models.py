62ae5121e399700ec80a42919247014c8f20af99
rohit <rohit.v@dvara.com>
2024-08-08 15:38:33 +0530

    limit decrased

--- testapp/models.py
+++ testapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    Title = models.CharField(max_length=100)
+    Title = models.CharField(max_length=200)
     Content = models.TextField(max_length=500)
     Author = models.CharField(max_length=50)
